c89ab5419e90955fd3b744c4e616df14c694b7a3
NotRedFox <109942259+NotRedFox@users.noreply.github.com>
2026-03-08 19:53:03 +1000

    Add files via upload
    Extremely basic python I made.

---
+++ Basic Python.py
@@ -0,0 +1,8 @@
+name = input("Hello what is your name ")
+print(f"Nice to meet you,{name}!")
+age = int(input("How old are you "))
+bot_age = 4
+age_difference = age - bot_age
+print (f"You are {age_difference} years older than me. I'm {bot_age} years old!")
+color = input("What is your favorite colour? ")
+print(f"Oh, {color} is cool colour")
